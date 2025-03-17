b4d020d162f4f135536b239ac0d186c72f8f3f4f
krishna0890227 <s.kr.acharya@gmail.com>
2025-03-17 23:36:22 +0000

    Answer are checked

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type")==="submit") {
                 alert("You clicked submit button!")
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,10 +38,29 @@ function runGame(gameType) {
 
 
 function checkAnswer() {
+    let useranswer= parseInt(document.getElementById("answer-box").value);
+   let correctAnswer=calculateCorrectAnswer();
+   iscorrect = useranswer === correctAnswer[0]
+   if(iscorrect) {
+    alert(`Yes you got write answer ${correctAnswer[0]} :D`)
 
+   } else {
+    alert(`Oops! Your answer is ${useranswer}!. But the right answer is ${correctAnswer[0]}`);
+   }
+   runGame(calculateCorrectAnswer[1]);
 }
 
 function calculateCorrectAnswer() {
+    let operand1= parseInt(document.getElementById("operand1").innerText)
+    let operand2= parseInt(document.getElementById("operand2").innerText)
+    let operator= document.getElementById("operator").innerText;
+    
+    if(operator === "+") {
+        return ([operand1 + operand2, "addition"])
+    } else {
+        alert("You selected wrong game question");
+        throw `unknown game type ${operator}! Game is Aborting`;
+    }
 
 }
 
