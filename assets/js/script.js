152e79d096ad972ca0d485985f4bbddbcf319fd2
krishna0890227 <s.kr.acharya@gmail.com>
2025-03-17 21:59:51 +0000

    Js basic function structured

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,34 @@
 /*   JavaScript code here  */
 console.log("connected");
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displayMultiplicationQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
