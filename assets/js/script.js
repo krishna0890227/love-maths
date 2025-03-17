5bf0e55e135ec86805caf8d408857e46f71210dd
krishna0890227 <s.kr.acharya@gmail.com>
2025-03-17 22:57:27 +0000

    addition function is setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit button!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType} button` )
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
 /** The main function is loaded and 
  * it call to runGame that generate two random numbers  */
 
-function runGame() {
+function runGame(gameType) {
     num1=Math.floor(Math.random()*25) +1;
     num2= Math.floor(Math.random()*25) + 1;
+
+    if(gameType==="addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert("You selected wrong game question");
+        throw `unknown game type ${gameType}! Game is Aborting`;
+    }
+
 
 }
 
@@ -43,7 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    console.log(operand1, operand2)
+    document.getElementById("operand1").textContent= operand1;
+    document.getElementById("operand2").textContent= operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
