e081660ccfbced0269ca32e04b9045990ad60af5
krishna0890227 <s.kr.acharya@gmail.com>
2025-03-17 22:26:54 +0000

    Basic Eventlister buttons setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 /*   JavaScript code here  */
 console.log("connected");
 
+document.addEventListener("DOMContentLoaded", function() {
+    buttons= document.getElementsByTagName("button"); 
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type")==="submit") {
+                alert("You clicked submit button!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType} button` )
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -18,7 +33,7 @@ function incrementScore() {
 }
 
 function incrementWrongAnswer() {
-    
+
 }
 
 function displayAdditionQuestion() {
